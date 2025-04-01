432795d9e75648515eb1f2d050a2aced9aaeb4c6
Pawel Przybyla <pawel.przybyla2005@gmail.com>
2025-04-01 16:53:50 +0200

    implemented the function convertDouble

--- ex00/convert.cpp
+++ ex00/convert.cpp
@@ -6,16 +6,63 @@
 /*   By: paprzyby <paprzyby@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/19 16:34:31 by paprzyby          #+#    #+#             */
-/*   Updated: 2025/04/01 16:49:42 by paprzyby         ###   ########.fr       */
+/*   Updated: 2025/04/01 16:52:56 by paprzyby         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ScalarConverter.hpp"
 
-//void	convertDouble(std::string string)
-//{
+void	convertDouble(std::string string)
+{
+	std::string::iterator	str = string.begin();
+	int						dots_count = 0;
 
-//}
+	if (*str == '+' || *str == '-')
+	{
+		str++;
+	}
+	while (str != string.end())
+	{
+		if (*str == '.')
+		{
+			dots_count++;
+		}
+		else if (!std::isdigit(*str))
+		{
+			std::cerr << "This argument is not valid" << std::endl;
+			return ;
+		}
+		str++;
+	}
+	if (dots_count != 1)
+	{
+		std::cerr << "This argument is not valid" << std::endl;
+		return ;
+	}
+
+	double num = std::stod(string);
+	if (isprint(num))
+	{
+		std::cout << "char: '" << static_cast<char>(num) << "'" << std::endl;
+	}
+	else
+	{
+		if (num < 0 || num > 255)
+			std::cout << "char: impossible" << std::endl;
+		else
+			std::cout << "char: " << "Non displayable" << std::endl;
+	}
+	if (num > INT_MAX)
+	{
+		std::cout << "int: impossible" << std::endl;
+	}
+	else
+	{
+		std::cout << "int: " << static_cast<int>(num) << std::endl;
+	}
+	std::cout << "float: " << static_cast<float>(num) << ".0f" << std::endl;
+	std::cout << "double: " << num << ".0" << std::endl;
+}
 
 void	convertFloat(std::string string)
 {
@@ -178,7 +225,7 @@ void ScalarConverter::convert(std::string const &literal)
 	}
 	else if (literal.find(".") != std::string::npos)
 	{
-		std::cout << "double" << std::endl;
+		convertDouble(literal);
 	}
 	else
 	{
