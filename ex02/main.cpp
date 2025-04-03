c004df201f77414a01e88a4becc8e4e5c51ea430
Pawel Przybyla <pawel.przybyla2005@gmail.com>
2025-04-03 17:48:37 +0200

    implemented the function generate()

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,16 +6,26 @@
 /*   By: paprzyby <paprzyby@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 17:17:43 by paprzyby          #+#    #+#             */
-/*   Updated: 2025/04/03 17:31:28 by paprzyby         ###   ########.fr       */
+/*   Updated: 2025/04/03 17:47:54 by paprzyby         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Base.hpp"
+#include "A.hpp"
+#include "B.hpp"
+#include "C.hpp"
 
-//Base	*generate()
-//{
+Base	*generate()
+{
+	int randomValue = rand() % 100;
 
-//}
+	if (randomValue < 25)
+		return (new (A));
+	else if (randomValue < 50)
+		return (new (B));
+	else
+		return (new (C));
+}
 
 //void	identify(Base *p)
 //{
@@ -29,5 +39,10 @@
 
 int	main()
 {
+	srand(time(NULL));
+
+	Base	*base;
+
+	base = generate();
 	return (0);
 }
