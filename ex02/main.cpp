dd46c5cb0f6063e89a93fee341dd7b61badec75c
Pawel Przybyla <pawel.przybyla2005@gmail.com>
2025-04-03 18:19:45 +0200

    finished ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: paprzyby <paprzyby@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/03 17:17:43 by paprzyby          #+#    #+#             */
-/*   Updated: 2025/04/03 17:47:54 by paprzyby         ###   ########.fr       */
+/*   Updated: 2025/04/03 18:19:15 by paprzyby         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,35 +14,68 @@
 #include "A.hpp"
 #include "B.hpp"
 #include "C.hpp"
+#include <random>
 
 Base	*generate()
 {
-	int randomValue = rand() % 100;
+	std::random_device				device;
+	std::mt19937					generate(device());
+	std::uniform_int_distribution	range(1, 3);
+	int								random = range(generate);
 
-	if (randomValue < 25)
-		return (new (A));
-	else if (randomValue < 50)
-		return (new (B));
+	if (random == 1)
+		return (new A);
+	else if (random == 2)
+		return (new B);
 	else
-		return (new (C));
+		return (new C);
 }
 
-//void	identify(Base *p)
-//{
+void	identify(Base *p)
+{
+	if (dynamic_cast<A*>(p))
+		std::cout << "A" << std::endl;
+	else if (dynamic_cast<B*>(p))
+		std::cout << "B" << std::endl;
+	else
+		std::cout << "C" << std::endl;
+}
 
-//}
-
-//void	identify(Base &p)
-//{
-
-//}
+void	identify(Base &p)
+{
+	try
+	{
+		(void)dynamic_cast<A&>(p);
+		std::cout << "A" << std::endl;
+	}
+	catch (std::bad_cast &)
+	{
+		try
+		{
+			(void)dynamic_cast<B&>(p);
+			std::cout << "B" << std::endl;
+		}
+		catch (std::bad_cast &)
+		{
+			try
+			{
+				(void)dynamic_cast<C&>(p);
+				std::cout << "C" << std::endl;
+			}
+			catch (std::bad_cast &)
+			{
+				std::cout << "Unknown type" << std::endl;
+			}
+		}
+	}
+}
 
 int	main()
 {
-	srand(time(NULL));
-
 	Base	*base;
 
 	base = generate();
+	identify(base);
+	identify(*base);
 	return (0);
 }
